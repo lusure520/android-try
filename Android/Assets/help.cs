115eda63dfa7e35f7a9dd706d1c5409b8d03768c
ZACH\Zach <40458689@qq.com>
2018-05-18 20:15:18 +1200

    Button
    GUIButton

---
+++ Android/Assets/help.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+
+public class help : MonoBehaviour
+{
+
+    // Use this for initialization  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+
+    }
+    public bool WindowShow = false;
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(310, 10, 80, 30), "Help"))
+        {
+                WindowShow = true;
+           
+        }
+        if (WindowShow)
+        {
+            GUI.Box(new Rect(300, 20, 200, 200), "Help");
+            GUILayout.BeginArea(new Rect(300, 40, 200, 200));
+            GUILayout.Label("1. ");
+            GUILayout.Label("2. ");
+            GUILayout.Label("3. ");
+            GUILayout.Label("4. ");
+            GUILayout.EndArea();
+            if (GUI.Button(new Rect(360, 170, 80, 30), "Close"))
+            {
+                WindowShow = false;
+            }
+        }
+    }
+}
